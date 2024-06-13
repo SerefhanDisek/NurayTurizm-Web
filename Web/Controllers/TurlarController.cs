6812a5aab7c6c9162c51a00429359772d00a7821
SerefhanDisek <glorius488@gmail.com>
2024-06-14 01:17:34 +0300

    Turlar

---
+++ Web/Controllers/TurlarController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class TurlarController : Controller
+    {
+        public IActionResult Uludag()
+        {
+            return View();
+        }
+
+        public IActionResult Karadeniz()
+        {
+            return View();
+        }
+
+        public IActionResult Pamukkale()
+        {
+            return View();
+        }
+
+        public IActionResult Kaplica()
+        {
+            return View();
+        }
+
+        public IActionResult Gap()
+        {
+            return View();
+        }
+
+        public IActionResult Gunubirlik()
+        {
+            return View();
+        }
+
+        public IActionResult EgeAkdeniz()
+        {
+            return View();
+        }
+
+        public IActionResult Istanbul()
+        {
+            return View();
+        }
+
+        public IActionResult Kapadokya()
+        {
+            return View();
+        }
+
+        public IActionResult Balkan()
+        {
+            return View();
+        }
+
+        public IActionResult Umre()
+        {
+            return View();
+        }
+
+        public IActionResult Italya()
+        {
+            return View();
+        }
+
+        public IActionResult EgeAdalari()
+        {
+            return View();
+        }
+
+        public IActionResult BatumGurcistan()
+        {
+            return View();
+        }
+
+        public IActionResult Azerbaycan()
+        {
+            return View();
+        }
+
+        public IActionResult Dubai()
+        {
+            return View();
+        }
+    }
+}
+

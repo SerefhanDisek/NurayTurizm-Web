3a635af18ef6a705b6ade82b003042ef70ec22f5
SerefhanDisek <glorius488@gmail.com>
2024-06-15 01:27:22 +0300

    GeziRehberi2

--- Web/Controllers/GeziRehberiController.cs
+++ Web/Controllers/GeziRehberiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Web.Models;
 
 namespace Web.Controllers
@@ -16,6 +17,34 @@ namespace Web.Controllers
             };
 
             return View(photos);
+        }
+        public IActionResult Videolar()
+        {
+            var videos = new List<Video>
+            {
+                new Video { Id = 1, Title = "Video 1", Description = "Bu, seyahatlerimizden bir videodur.", VideoUrl = "/videos/video1.mp4" },
+                new Video { Id = 2, Title = "Video 2", Description = "Bu, seyahatlerimizden bir videodur.", VideoUrl = "/videos/video2.mp4" },
+                new Video { Id = 3, Title = "Video 3", Description = "Bu, seyahatlerimizden bir videodur.", VideoUrl = "/videos/video3.mp4" }
+            };
+
+            return View(videos);
+        }
+
+        public IActionResult Haritalar()
+        {
+            return View();
+        }
+
+        public IActionResult Kameramizdan()
+        {
+            var cameras = new List<Camera>
+            {
+                new Camera { Id = 1, Title = "Kamera 1", Description = "Bu, seyahatlerimizden bir görüntüdür.", ImageUrl = "/img/camera1.jpg" },
+                new Camera { Id = 2, Title = "Kamera 2", Description = "Bu, seyahatlerimizden bir görüntüdür.", ImageUrl = "/img/camera2.jpg" },
+                new Camera { Id = 3, Title = "Kamera 3", Description = "Bu, seyahatlerimizden bir görüntüdür.", ImageUrl = "/img/camera3.jpg" }
+            };
+
+            return View(cameras);
         }
     }
 }

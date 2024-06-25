4da0b07c20820f4b0c90f6842dbf40da659a26d5
SerefhanDisek <glorius488@gmail.com>
2024-06-25 22:59:29 +0300

    arama

---
+++ Web/Controllers/AramaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Web.Controllers
+{
+    public class AramaController : Controller
+    {
+        // Bu örnekte bir liste üzerinden arama yapıyoruz.
+        private static List<string> ornekVeri = new List<string>
+        {
+            "Uludağ Turları",
+            "Karadeniz Turları",
+            "Pamukkale Turları",
+            "Kapadokya Turları",
+            "GAP Turları",
+            "Günübirlik Turlar",
+            "Ege-Akdeniz Turları",
+            "İstanbul Turları",
+            "Balkan Turları",
+            "Umre Organizasyonları",
+            "İtalya Turları",
+            "Ege Adaları Turları",
+            "Batum - Gürcistan Turları",
+            "Azerbaycan Turları",
+            "Dubai Turları"
+        };
+
+        [HttpGet]
+        public IActionResult Index(string q)
+        {
+            var results = ornekVeri.Where(v => v.Contains(q, StringComparison.OrdinalIgnoreCase)).ToList();
+            return View(results);
+        }
+    }
+}
